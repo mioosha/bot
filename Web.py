6077ec3c3e1af2f40b75f7f94f6a8d6ca4ea9f48
mioosha <magorety14@gmail.com>
2025-10-26 22:50:56 +0300

    Update Web.py

--- Web.py
+++ Web.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_id = request.args.get("user_id", "Неизвестный пользователь")
-    return render_template("index.html", user_id=user_id)
+    user_id = request.args.get('user_id', '...')
+    return render_template('index.html', user_id=user_id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
